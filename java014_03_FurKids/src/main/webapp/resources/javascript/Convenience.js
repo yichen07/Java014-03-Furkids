3793f47ac13af774fe9466734e65a5b9667b2f38
pilimou <soulms55@gmail.com>
2020-09-11 17:08:20 +0800

    修改我的前端again

--- java014_03_FurKids/src/main/webapp/resources/javascript/Convenience.js
+++ java014_03_FurKids/src/main/webapp/resources/javascript/Convenience.js
@@ -1,18 +1,35 @@
 $(document).ready(function(){
 
-	
-		$('.revise').click(function(){		
+		//按下修改時
+		$('.revise').click(function(){	
+			
 			display = $('.ccc').css('display');
-		
+				
 			if(display == 'none'){
 				$('.ccc').css('display','block');
 				$('.ddd').css('display','none');
-			}
-					
-			if(display == 'block'){	
+				$('.revise').html('取消修改');
+			} else if(display == 'block'){	
 				$('.ccc').css('display','none');
 				$('.ddd').css('display','block');
+				$('.revise').html('修改');
 			} 
+					
+			if($('.delete').css('display') == 'block'){
+				$('.delete').css('display','none');
+			} else if ($('.delete').css('display') == 'none'){
+				$('.delete').css('display','block');
+			}
+					
+			//關閉modal前呼叫
+			$(".mmm").on("hidden.bs.modal",function(e){
+  				$('.ccc').css('display','none');
+				$('.ddd').css('display','block');
+				$('.delete').css('display','block');
+				$('.revise').html('修改');				
+			});
+			
+			
 			
 		});	
 					
